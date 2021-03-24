20a5ee333f3c5cc38d531f4dc61b60a4759cb102
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2021-03-24 08:44:34 -0300

    can now get terminal script arguments

--- client/index.js
+++ client/index.js
@@ -1,10 +1,21 @@
+/*
+node index.js \
+    ---username grochavieira \
+    --room sala01 \
+    --hostUri localhost
+*/
+
 import Events from "events";
+import CliConfig from "./src/cliConfig.js";
 import TerminalController from "./src/terminalController.js";
 
+const [nodePath, filePath, ...commands] = process.argv;
+const config = CliConfig.parseArguments(commands);
+console.log("config ", config);
 // vai trafegar entre as camadas de código
 // para emitir os eventos necessários para
 // atualizar a tela e outra ações
 const componentEmitter = new Events();
 
-const controller = new TerminalController();
-await controller.initializeTable(componentEmitter);
+// const controller = new TerminalController();
+// await controller.initializeTable(componentEmitter);
