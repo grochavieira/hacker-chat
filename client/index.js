2c595068882432eb7789e8dabc86185b36dba414
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2021-03-25 08:11:16 -0300

    created socket connection in client and server

--- client/index.js
+++ client/index.js
@@ -7,15 +7,17 @@ node index.js \
 
 import Events from "events";
 import CliConfig from "./src/cliConfig.js";
+import SocketClient from "./src/socket.js";
 import TerminalController from "./src/terminalController.js";
 
 const [nodePath, filePath, ...commands] = process.argv;
 const config = CliConfig.parseArguments(commands);
-console.log("config ", config);
+
 // vai trafegar entre as camadas de código
 // para emitir os eventos necessários para
 // atualizar a tela e outra ações
 const componentEmitter = new Events();
-
+const socketClient = new SocketClient(config);
+await socketClient.initialize();
 // const controller = new TerminalController();
 // await controller.initializeTable(componentEmitter);
